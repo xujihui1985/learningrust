f62b50c844df8a08e7fed2f25f8defec82296767
慕陶 <jihui.xjh@antfin.com>
2020-01-23 11:13:21 +0800

    update

---
+++ procedual_macro_demo/hello-world-derive/src/lib.rs
@@ -0,0 +1,23 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
+
+#[proc_macro_derive(HelloWorld)]
+pub fn hello_world(input: TokenStream) -> TokenStream {
+  let ast = parse_macro_input!(input as DeriveInput);
+  let gen = impl_hello_world(&ast);
+  TokenStream::from(gen)
+}
+
+fn impl_hello_world(ast: &syn::DeriveInput) -> TokenStream {
+  let name = &ast.ident;
+  let expand = quote!{
+    impl HelloWorld for #name {
+      fn hello_world(&self) {
+        println!("hello {}", stringify!(#name));
+      }
+    }
+  };
+  expand.into()
+}
---
+++ procedual_macro_demo/src/main.rs
@@ -0,0 +1,14 @@
+extern crate hello_world_derive;
+use hello_world_derive::HelloWorld;
+
+trait HelloWorld {
+    fn hello_world(&self);
+}
+
+#[derive(HelloWorld)]
+struct World;
+
+fn main() {
+    let w = World{};
+    w.hello_world();
+}
